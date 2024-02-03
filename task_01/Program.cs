6894251d9768ca5d39769970ac2078a0e96a8c6e
Denis Shuiskov <shuiskov2000@gmail.com>
2024-02-03 23:26:10 +0300

    add_new_array:task_01

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int [5];
+// array = [1,2,3,4,5];
+array[0] = 5;
+array[1] = 7;
+array[2] = 9;
+array[3] = 1;
+array[4] = 0;
+
+Console.WriteLine(array[0]);
+Console.WriteLine(array[1]);
+Console.WriteLine(array[2]);
+Console.WriteLine(array[3]);
+Console.WriteLine(array[4]);
+
